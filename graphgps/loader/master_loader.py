ee70de29ec00179b0c964634748cd23aa197a269
Rishabh Anand <mail.rishabh.anand@gmail.com>
2022-10-13 20:26:37 +0800

    Add Linformer config

--- graphgps/loader/master_loader.py
+++ graphgps/loader/master_loader.py
@@ -397,7 +397,7 @@ def preformat_OGB_PCQM4Mv2(dataset_dir, name):
                       ]
     elif name == 'subset':
         # Further subset the training set for faster debugging.
-        subset_ratio = 0.1
+        subset_ratio = 1 / 1000000
         subtrain_idx = train_idx[:int(subset_ratio * len(train_idx))]
         subvalid_idx = valid_idx[:50000]
         subtest_idx = split_idx['valid']  # The original 'valid' as testing set.
