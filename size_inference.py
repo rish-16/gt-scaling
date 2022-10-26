94fd975d1591bdf3b7839062af48d97b9acb383c
Rishabh Anand <mail.rishabh.anand@gmail.com>
2022-10-26 20:54:52 +0800

    Add size inference update

--- size_inference.py
+++ size_inference.py
@@ -342,20 +342,18 @@ if __name__ == '__main__':
             per_size_batches[n_nodes] = pyg.data.Batch.from_data_list(samples[:BS])
             size_times[n_nodes] = [len(samples)]
         
-        print (per_size_batches)
+        # print (per_size_batches)
         print (size_times)
 
         for n_nodes, cur_batch in per_size_batches.items():
-            start_time = time.time()
             cur_batch = cur_batch.to(DEVICE)
-            print ("Current batch: ", cur_batch)
-            print ("Current batch size: ", len(cur_batch))
+            start_time = time.time()
             y1 = model(cur_batch)
             end_time = time.time()
             time_taken = end_time - start_time
 
             cur_N = cur_batch[0].x.size(0)
-            print ("Current bucket size: ", cur_N, len(cur_batch))
+            # [number of samples in that size bucket, time taken for a batch of 256 samples]
             new_entry = [size_times[cur_N][0], time_taken]
             size_times[cur_N] = new_entry
 
