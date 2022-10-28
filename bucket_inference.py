771af4fe0b875206545c0c6ff709cf89e5444df3
Rishabh Anand <mail.rishabh.anand@gmail.com>
2022-10-28 21:49:51 +0800

    Update train loader index

--- bucket_inference.py
+++ bucket_inference.py
@@ -15,6 +15,36 @@ import torch
 from torch_geometric.data import InMemoryDataset
 from torch_geometric.data import Data
 
+import os, logging, datetime, shutil, time
+import os.path as osp
+from ogb.utils import smiles2graph
+from ogb.utils.torch_util import replace_numpy_with_torchtensor
+from ogb.utils.url import decide_download, download_url, extract_zip
+import pandas as pd
+from tqdm import tqdm
+import torch, pickle
+
+from graphgps.optimizer.extra_optimizers import ExtendedSchedulerConfig
+from torch_geometric.graphgym.cmd_args import parse_args
+from torch_geometric.graphgym.config import (cfg, dump_cfg, set_agg_dir, set_cfg, load_cfg, makedirs_rm_exist)
+from torch_geometric.graphgym.loader import create_loader
+from torch_geometric.graphgym.logger import set_printing
+from torch_geometric.graphgym.optim import create_optimizer, create_scheduler, OptimizerConfig
+from torch_geometric.graphgym.model_builder import create_model
+from torch_geometric.graphgym.train import train
+from torch_geometric.graphgym.utils.agg_runs import agg_runs
+from torch_geometric.graphgym.utils.comp_budget import params_count
+from torch_geometric.graphgym.utils.device import auto_select_device
+from torch_geometric.graphgym.register import train_dict
+from torch_geometric.graphgym.loss import compute_loss
+from torch_geometric import seed_everything
+
+from graphgps.finetuning import load_pretrained_model_cfg, init_model_from_pretrained
+from graphgps.logger import create_logger
+
+import torch_geometric as pyg
+from pprint import pprint
+
 
 class PygPCQM4Mv2Dataset(InMemoryDataset):
     def __init__(self, root='../datasets/', smiles2graph=smiles2graph, transform=None, pre_transform=None):
@@ -107,10 +137,73 @@ class PygPCQM4Mv2Dataset(InMemoryDataset):
         split_dict = replace_numpy_with_torchtensor(torch.load(osp.join(self.root, 'split_dict.pt')))
         return split_dict
 
-# def PREPROCESS(data):
+def run_loop_settings():
+    """Create main loop execution settings based on the current cfg.
+
+    Configures the main execution loop to run in one of two modes:
+    1. 'multi-seed' - Reproduces default behaviour of GraphGym when
+        args.repeats controls how many times the experiment run is repeated.
+        Each iteration is executed with a random seed set to an increment from
+        the previous one, starting at initial cfg.seed.
+    2. 'multi-split' - Executes the experiment run over multiple dataset splits,
+        these can be multiple CV splits or multiple standard splits. The random
+        seed is reset to the initial cfg.seed value for each run iteration.
+
+    Returns:
+        List of run IDs for each loop iteration
+        List of rng seeds to loop over
+        List of dataset split indices to loop over
+    """
+    if len(cfg.run_multiple_splits) == 0:
+        # 'multi-seed' run mode
+        num_iterations = args.repeat
+        seeds = [cfg.seed + x for x in range(num_iterations)]
+        split_indices = [cfg.dataset.split_index] * num_iterations
+        run_ids = seeds
+    else:
+        # 'multi-split' run mode
+        if args.repeat != 1:
+            raise NotImplementedError("Running multiple repeats of multiple "
+                                      "splits in one run is not supported.")
+        num_iterations = len(cfg.run_multiple_splits)
+        seeds = [cfg.seed] * num_iterations
+        split_indices = cfg.run_multiple_splits
+        run_ids = split_indices
+
+    return run_ids, seeds, split_indices
+
+def custom_set_out_dir(cfg, cfg_fname, name_tag):
+    """Set custom main output directory path to cfg.
+    Include the config filename and name_tag in the new :obj:`cfg.out_dir`.
+
+    Args:
+        cfg (CfgNode): Configuration node
+        cfg_fname (string): Filename for the yaml format configuration file
+        name_tag (string): Additional name tag to identify this execution of the
+            configuration file, specified in :obj:`cfg.name_tag`
+    """
+    run_name = os.path.splitext(os.path.basename(cfg_fname))[0]
+    run_name += f"-{name_tag}" if name_tag else ""
+    cfg.out_dir = os.path.join(cfg.out_dir, run_name)
+
+def custom_set_run_dir(cfg, run_id):
+    """Custom output directory naming for each experiment run.
+
+    Args:
+        cfg (CfgNode): Configuration node
+        run_id (int): Main for-loop iter id (the random seed or dataset split)
+    """
+    cfg.run_dir = os.path.join(cfg.out_dir, str(run_id))
+    # Make output directory
+    if cfg.train.auto_resume:
+        os.makedirs(cfg.run_dir, exist_ok=True)
+    else:
+        makedirs_rm_exist(cfg.run_dir)    
 
 if __name__ == '__main__':
     dataset = PygPCQM4Mv2Dataset()
+    dataset = dataset[:2000]
+    train_loader = pyg.loader.DataLoader(dataset, batch_size=1, shuffle=False)
     print(dataset)
     # print(dataset.data.edge_index)
     # print(dataset.data.edge_index.shape)
@@ -119,9 +212,77 @@ if __name__ == '__main__':
     # print(dataset[100].y)
     # print(dataset.get_idx_split())
 
-    per_bucket_samples = {}
+    # for i in range(10):
+        # print (dataset[i])
 
-    for i in range(10):
-        print (dataset[i])
+    per_size_batches = {}
+    print ("Size of train loader:", train_loader)
+    for i, data_batch in enumerate(train_loader):
+        sample = data_batch.to_data_list()[0] # only works with batch size of 1 in the yaml
+        n_nodes = sample.x.size(0)
+        if n_nodes not in per_size_batches:
+            per_size_batches[n_nodes] = sample
+        else:
+            pass
+
+    # with open('bucket_inference_pcqfull.pickle', 'wb') as f:
+    #     pickle.dump(per_size_batches, f, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    # print ("Saved pickle file")
+    print (per_size_batches.keys())
+
+    # args = parse_args()
+    # # Load config file
+    # set_cfg(cfg)
+    # load_cfg(cfg, args)
+    # # custom_set_out_dir(cfg, args.cfg_file, cfg.name_tag)
+    # dump_cfg(cfg)
+    # # Set Pytorch environment
+    # torch.set_num_threads(cfg.num_threads)
+    # # gpu_dev = str(input("Enter GPU device: "))
+    # # Repeat for multiple experiment runs
+    # for run_id, seed, split_index in zip(*run_loop_settings()):
+    #     # Set configurations for each run
+    #     # custom_set_run_dir(cfg, run_id)
+    #     set_printing()
+    #     cfg.dataset.split_index = split_index
+    #     cfg.seed = seed
+    #     cfg.run_id = run_id
+    #     seed_everything(cfg.seed)
+        
+    #     # auto_select_device()
+    #     BS = 64
+    #     DEVICE = f'cuda:0'
+    #     cfg.device = DEVICE
+
+
+    #     if cfg.pretrained.dir:
+    #         cfg = load_pretrained_model_cfg(cfg)
+
+    #     logging.info(f"    Starting now: {datetime.datetime.now()}")
+
+    #     model = create_model()
+
+    #     cfg.params = params_count(model)
+    #     logging.info('Num parameters: %s', cfg.params)
+
+    #     TIMINGS = {}
+    #     for n_nodes, sample in per_size_batches.items():
+    #         temp = []
+    #         batch_list = [sample for _ in range(BS)]
+    #         batch = pyg.data.Batch.from_data_list(batch_list)
+    #         batch.to(DEVICE)
+            
+    #         with torch.no_grad():
+    #             start = time.time()
+    #             y1 = model(batch)
+    #             end = time.time()
+    #             del batch
+    #             TIMINGS[n_nodes] = end - start
+
+    #     pprint (TIMINGS)
+
+    #     with open("BIGBIRD_TIMINGS.json", "a") as f:
+    #         json.dump(TIMINGS, f)
 
     
