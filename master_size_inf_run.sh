c84bf9edbebc0d6b38b9e8be9b72d38b0995a4dd
rish-16 <mail.rishabh.anand@gmail.com>
2022-10-28 18:05:29 +0800

    Add results from new ZINC runs

---
+++ master_size_inf_run.sh
@@ -0,0 +1,3 @@
+python size_inference_only.py --cfg configs/GPS/scaling-buckets/pcqm4m-bucket-GPSmedium-Performer.yaml 
+python size_inference_only.py --cfg configs/GPS/scaling-buckets/pcqm4m-bucket-GPSmedium-Transformer.yaml
+python size_inference_only.py --cfg configs/GPS/scaling-buckets/pcqm4m-bucket-GPSmedium-BigBird.yaml 
